ddce583e583b7a82bd228e9adb811416a9b756bd
Hatim P <hpartapur@gmail.com>
2022-08-05 17:44:12 +0300

    removed automatic hint

--- script.js
+++ script.js
@@ -29,13 +29,13 @@ console.log(rightGuessString)
 // When the user clicks on <span> (x), close the modal
  span.onclick = function() {
    modal.style.display = "none";
-     firsthint()
+//     firsthint()
  }
  // When the user clicks anywhere outside of the modal, close it
  window.onclick = function(event) {
    if (event.target == modal) {
      modal.style.display = "none";
-    firsthint()
+//    firsthint()
    }
  }
 
