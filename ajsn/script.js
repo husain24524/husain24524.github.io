f555405dddb10baa2e1a9c93c179539b3fc8acec
hpartapur <83201298+hpartapur@users.noreply.github.com>
2023-04-29 17:41:48 +0300

    abandoned table format and switched to images

--- ajsn/script.js
+++ ajsn/script.js
@@ -13,44 +13,10 @@ function modalDelay(){
     });
 }
 
-function cookietotable(){
+function chooseschedule (day){
     var group = getCookie("group");
-    //search json for correct group json
-
-    //create html table from json and adds to page
-    convertJsonToBootstrapTable(); //NEEDS PARAMETER OF JSON, AND PARAMETER OF TABLE ID
-
-
-
-
-}
- 
-function convertJsonToBootstrapTable(json,id){
-    //Get the headers from JSON data
-    var headers = Object.keys(json[0]);
-     
-    //Prepare html header
-    var headerRowHTML='<tr>';
-    for(var i=0;i<headers.length;i++){
-        headerRowHTML+='<th>'+headers[i]+'</th>';
+    if (group != undefined){
+        var imgname =group+day+".png";
+        document.getElementById("schedule").src=imgname;
     }
-    headerRowHTML+='</tr>';       
-     
-    //Prepare all the employee records as HTML
-    var allRecordsHTML='';
-    for(var i=0;i<json.length;i++){
-     
-        //Prepare html row
-        allRecordsHTML+='<tr>';
-        for(var j=0;j<headers.length;j++){
-            var header=headers[j];
-            allRecordsHTML+='<td>'+json[i][header]+'</td>';
-        }
-        allRecordsHTML+='</tr>';
-         
-    }
-     
-    //Append the table header and all records
-    var table=document.getElementById(id);
-    table.innerHTML=headerRowHTML + allRecordsHTML;
 }
